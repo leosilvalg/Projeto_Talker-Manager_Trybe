e7ef94441f31ad316c9160b0eae92dc28220f3ba
Leonardo Silva <leonardorc.geo@gmail.com>
2022-07-09 11:19:01 -0300

    Começando. Requisito 01

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs');
+
+const data = './talker.json';
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,6 +13,11 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', (request, response) => {
+  const talkerPersons = JSON.parse(fs.readFileSync(data));
+  response.status(HTTP_OK_STATUS).send(talkerPersons);
 });
 
 app.listen(PORT, () => {
